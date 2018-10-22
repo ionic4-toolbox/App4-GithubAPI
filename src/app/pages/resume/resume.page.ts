38ce557c3d015c3ebd0359a3031e585585a0adb4
Gary Großgarten <gary.grossgarten@gmail.com>
2018-10-23 00:46:22 +0200

    animations

--- src/app/pages/resume/resume.page.ts
+++ src/app/pages/resume/resume.page.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { UserDataService } from '../../user-data.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.page.html',
   styleUrls: ['./resume.page.scss'],
+  animations: [
+    trigger('about', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('225ms 150ms', style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('exp', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('225ms 250ms', style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('ed', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('225ms 350ms', style({ opacity: 1 }))
+      ])
+    ]),
+  ]
 })
 export class ResumePage implements OnInit {
 
